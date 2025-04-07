628905912a99187821466596aee5f5c24110b5f1
Gayatri R <gayatrirajgor@gmail.com>
2025-04-07 19:25:48 +0100

    created new pages

---
+++ src/pages/OraclePage.jsx
@@ -0,0 +1,13 @@
+// OraclePage.jsx
+import React from "react"
+import OracleCard from "../components/OracleCard"
+import OracleChat from "../components/OracleChat"
+
+const OraclePage = () => (
+  <>
+    <OracleCard />
+    <OracleChat />
+  </>
+)
+
+export default OraclePage
